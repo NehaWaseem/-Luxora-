8420b540711f549be3856e80d4d8b919a7f63c3c
N <nehawaseem846@gmail.com>
2025-01-02 03:27:09 +0500

    Signup Flow Commit

--- hotelms/src/pages/SignupForm.js
+++ hotelms/src/pages/SignupForm.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './SignupForm.css';
+import { useNavigate } from 'react-router-dom';
 
 const SignupForm = () => {
+
+  const navigate = useNavigate();
+
+  // handling function 
+  const handleSignup = (e) =>{
+
+    e.preventDefault();
+    alert('Account created successfully ! ');
+    navigate('/');
+  };
+
+
   return (
     <div className="signup-form">
       <h2>Create Account</h2>
@@ -15,11 +28,22 @@ const SignupForm = () => {
           <input type="checkbox" />
           <label>Accept the terms & privacy policy</label>
         </div>
-        <button type="submit" className="submit-button">Sign Up</button>
+        <button type="submit" className="submit-button" onClick={handleSignup}>Sign Up</button>
       </form>
+     
       <p className="login-link">
-        Already have an account? <a href="/login">Sign In</a>
+        Already have an account?{' '}
+        <a
+          href="/login"
+          onClick={(e) => {
+            e.preventDefault(); // Prevent default anchor behavior
+            navigate('/login'); 
+          }}
+        >
+          Sign In
+        </a>
       </p>
+
     </div>
   );
 };
