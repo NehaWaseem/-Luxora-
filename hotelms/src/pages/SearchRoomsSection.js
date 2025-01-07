7990b12dfab1282b318b685012b270ab1e93b1d5
N <nehawaseem846@gmail.com>
2025-01-07 17:37:40 +0500

    Search Rooms Options Bug Fix

--- hotelms/src/pages/SearchRoomsSection.js
+++ hotelms/src/pages/SearchRoomsSection.js
@@ -16,34 +16,6 @@ function SearchRoomsSection() {
       <div className="search-booking">
         <h2>Search for Rooms</h2>
        
-        <div className="search-options">
-          Check-in date
-          <input type="date" placeholder="Check-in Date" aria-label="Check-in Date" />
-          Check-out date
-          <input type="date" placeholder="Check-out Date" aria-label="Check-out Date" />
-          <select>
-            <option value="" disabled selected>
-              Select number of Guests
-            </option>
-            <option value="1">1 Guest</option>
-            <option value="2">2 Guests</option>
-            <option value="3">3 Guests</option>
-          </select>
-          <select>
-            <option value="" disabled selected>
-              Select number of beds
-            </option>
-            <option value="1">1 Bed</option>
-            <option value="2">2 Beds</option>
-          </select>
-          <select>
-            <option value="" disabled selected>
-              Select number of baths
-            </option>
-            <option value="1">1 Bath</option>
-            <option value="2">2 Baths</option>
-          </select>
-        </div>
         <div className="search-button-container">
           <button onClick = {handleSearch}>Search</button>
         </div>
